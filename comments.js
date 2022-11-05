2296a01b1567cb2d61185379c34bb5230af873ff
OlgaBuLL <108359930+OlgaBuLL@users.noreply.github.com>
2022-11-05 18:07:53 +0100

    first commit

---
+++ comments.js
@@ -0,0 +1,51 @@
+let comments = ["comment 1", "comment 2", "comment 3"];
+// let userComment;
+
+function finishTask(sender) {
+  sender.parentElement.classList.toggle("done");
+}
+function deleteComment() {
+  optionsString = "";
+}
+
+function sendComment() {
+  let userComment = checkSpam();
+  comments.push(userComment);
+  generateComments();
+  console.log(comments);
+}
+
+function checkSpam() {
+  let checkedSpam = document.getElementById("newComment").value;
+
+  checkedSpam = checkedSpam.replace(/viagra/gi, "***");
+  console.log(checkedSpam);
+
+  checkedSpam = checkedSpam.replace(/xxx/gi, "***");
+  console.log(checkedSpam);
+
+  let bitchSpam = checkedSpam.split(" ");
+
+  for (let i = 0; i < bitchSpam.length; i++) {
+    if (bitchSpam[i] === "bitch")
+      bitchSpam[i] = bitchSpam[i].replace(/i/gi, "*");
+
+    console.log(bitchSpam);
+  }
+  bitchSpam = bitchSpam.join(" ");
+  return bitchSpam;
+}
+
+function generateComments() {
+  let optionsString = "";
+  for (let comment of comments) {
+    // optionsString += `<div><input type="checkbox" onchange="finishTask(this)"><span>${comment}</span><button onchange="deleteComment()">Delete</button></div>`;
+    optionsString += `<div><span>${comment}</span><button onchange="deleteComment()">Delete</button></div>`;
+  }
+
+  document.getElementById("container").innerHTML = optionsString;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  generateComments();
+});
